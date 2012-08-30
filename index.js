a26a3e884cffbc866e9fb9c15c26aa7f25efd297
bobrik <ibobrik@gmail.com>
2012-08-30 15:59:18 +0400

    backpack-dump-log executable to dump log contents as "name offset length" lines

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function(module) {
     module.exports.Server      = require("./lib/Server");
     module.exports.FileManager = require("./lib/FileManager");
+    module.exports.FileMetaLog = require("./lib/FileMetaLog");
 })(module);
